07efc2ce0c88e65ecae94a37d4b64f40983658de
ys <644609238@qq.com>
2018-01-24 15:47:53 +0800

    ff

--- web/front/src/components/footer/footer.vue
+++ web/front/src/components/footer/footer.vue
@@ -1,10 +1,10 @@
 <template>
       <footer id="p_footer">
           <el-row :gutter="20">
-            <el-col :span="6"><div class="grid-content bg-purple"><i class="el-icon-goods" @click="toHome"></i>首页</div></el-col>
+            <el-col :span="6"><div class="grid-content bg-purple"><i class="el-icon-goods"></i>首页</div></el-col>
             <el-col :span="6"><div class="grid-content bg-purple"><i class="el-icon-edit-outline"></i>点餐</div></el-col>
-            <el-col :span="6"><div class="grid-content bg-purple"><i class="el-icon-edit" @click="toCart"></i>购物车</div></el-col>
-            <el-col :span="6"><div class="grid-content bg-purple"><i class="el-icon-goods" @click="toMine"></i>我的</div></el-col>
+            <el-col :span="6"><div class="grid-content bg-purple"><i class="el-icon-edit"></i>购物车</div></el-col>
+            <el-col :span="6"><div class="grid-content bg-purple"><i class="el-icon-goods"></i>我的</div></el-col>
           </el-row>
       </footer>
 
@@ -19,17 +19,6 @@
         return {
         }
       },
-      methods:{
-        toHome(){
-          this.$router.push({path:'/'})
-        },
-        toCart(){
-          this.$router.push({path:'/eicart'})
-        },
-        toMine(){
-          this.$router.push('mine')
-        }
-      }
 
     }
 </script>
